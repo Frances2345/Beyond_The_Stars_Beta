87ab726a37420b943aa40146622f91f174ab1896
renzoperalta-ship-it <renzo.peralta@tecsup.edu.pe>
2025-11-25 15:46:58 -0500

    Mirada del sprite, sprite jugador, astro trooper
    script de mirar al jugador y mouse

---
+++ Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Compensamos porque el sprite mira hacia ARRIBA (90°)
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
+    }
+}
---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class EnemyLookAtPlayer : MonoBehaviour
+{
+    public Transform sprite;
+    public Transform player;
+
+    void Update()
+    {
+        if (sprite == null || player == null) return;
+
+        Vector2 dir = player.position - sprite.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        sprite.rotation = Quaternion.Euler(0, 0, angle + 90f);
+    }
+}
