5262b63084e30a7e7b90e333ca73704730d8b2ea
Frances2345 <francesco.moreno@tecsup.edu.pe>
2025-12-09 17:01:56 -0500

    listo mode selector

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    
+    public AudioClip clickSoundClip;
+    public AudioClip quitSoundClip;
+
+    public AudioClip menuMusicClip;
+
+    private Dictionary<string, AudioClip> musicData = new Dictionary<string, AudioClip>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        PlayMenuMusic();
+    }
+
+    public void PlayMenuMusic()
+    {
+        AudioSource musicSource = GetComponent<AudioSource>();
+        if(musicSource == null )
+        {
+            musicSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        if(menuMusicClip != null && !musicSource.isPlaying)
+        {
+            musicSource.clip = menuMusicClip;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
+
+    public void PlayClickSound()
+    {
+        AudioSource audioSource = GetTemporaryAudioSource();
+        if (clickSoundClip != null)
+        {
+            audioSource.PlayOneShot(clickSoundClip);
+        }
+    }
+
+    public void PlayQuitSound()
+    {
+        AudioSource audioSource = GetTemporaryAudioSource();
+        if (quitSoundClip != null)
+        {
+            audioSource.PlayOneShot(quitSoundClip);
+        }
+
+    }
+
+    private AudioSource GetTemporaryAudioSource()
+    {
+        AudioSource audioSource = GetComponent<AudioSource> ();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+        audioSource.loop = false;
+        return audioSource;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
