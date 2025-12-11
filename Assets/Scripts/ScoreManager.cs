57302a86f88efd23ae26034b6fa91f8fc4d77897
Frances2345 <francesco.moreno@tecsup.edu.pe>
2025-12-11 05:09:30 -0500

    waffwafwa

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -4,8 +4,15 @@ using System;
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance { get; private set; }
+
     public event Action<int> OnScoreChanged;
+    public event Action<int> OnHighScoreChanged;
+
     private int currentScore = 0;
+    private int highScore = 0;
+
+    private const string HighScoreKey = "HighScore";
+
 
     void Awake()
     {
@@ -17,18 +24,49 @@ public class ScoreManager : MonoBehaviour
         Instance = this;
     }
 
-    public void AddScore(int amount)
+    void Start()
     {
-        currentScore += amount;
-        Debug.Log("Puntaje actual: " + currentScore);
+        // Carga el High Score guardado. Si no existe, inicia en 0.
+        highScore = PlayerPrefs.GetInt(HighScoreKey, 0);
 
+        OnHighScoreChanged?.Invoke(highScore);
+
+        currentScore = 0;
         OnScoreChanged?.Invoke(currentScore);
     }
 
-    public int GetScore()
+    public void AddScore(int amount)
     {
-        return currentScore;
+        if (amount <= 0) return;
+
+        currentScore += amount;
+        OnScoreChanged?.Invoke(currentScore);
+
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+
+            // Guarda el nuevo High Score
+            PlayerPrefs.SetInt(HighScoreKey, highScore);
+            PlayerPrefs.Save();
+
+            OnHighScoreChanged?.Invoke(highScore);
+            Debug.Log("¡Nuevo High Score guardado!: " + highScore);
+        }
     }
 
+    public int GetHighScore()
+    {
+        return highScore;
+    }
 
+    public void ResetHighScore()
+    {
+        PlayerPrefs.DeleteKey(HighScoreKey);
+        highScore = 0;
+        currentScore = 0;
+        OnHighScoreChanged?.Invoke(highScore);
+        OnScoreChanged?.Invoke(currentScore);
+        Debug.Log("High Score reseteado.");
+    }
 }
